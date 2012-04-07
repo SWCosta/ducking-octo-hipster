79a0d4872e39cf6f7a808b7e98f7e70889fea17b
Jan Paki <jan.paki@ressourcenmangel.de>
2012-04-07 03:54:08 +0200

    non working will paginate renderer class

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'hirb'
 
 gem 'devise'
 gem 'simple_form'
+gem 'will_paginate'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     flash.now[:alert] = "ich teste was"
-    @users = User.all
+    @users = User.order(:email).page(params[:page]).per_page(5)
   end
 
   def new
