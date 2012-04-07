d74f184cdf68a083afd4140677f799c6501ac6cb
Jan Paki <jan.paki@ressourcenmangel.de>
2012-04-08 01:47:43 +0200

    added user validation for files

--- app/models/bin/file.rb
+++ app/models/bin/file.rb
@@ -2,6 +2,7 @@ class Bin::File < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :name
+  validates_inclusion_of :user_id, :in => User.all.map(&:id)
 
   mount_uploader :file, FileUploader
 
