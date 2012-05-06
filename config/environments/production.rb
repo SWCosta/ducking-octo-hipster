8977be32910fadf352a976f6a1ee9210ce684551
Jan Paki <jan.paki@ressourcenmangel.de>
2012-05-06 23:17:15 +0200

    reverted action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Filemanager::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   
-  config.action_mailer.smtp_settings = { :enable_starttls_auto => true }
+  #config.action_mailer.smtp_settings = { :enable_starttls_auto => true }
 
   # Enable threaded mode
   # config.threadsafe!
